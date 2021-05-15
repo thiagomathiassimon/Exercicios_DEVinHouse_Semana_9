971e27743e832dc59a3738952f6fc72503895b86
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-05-14 21:18:53 -0300

    Exercicio_1

---
+++ Exercicio_1/respota.sql
@@ -0,0 +1,16 @@
+INSERT INTO paciente (nome, nascimento, email, telefone) VALUES 
+	('João', '2000-01-01', 'joao@gmail.com', '48988776655'),
+	('Juca', '2000-01-02', 'juca@gmail.com', '48988774433'),
+	('Juliana', '2000-01-03', 'juliana@gmail.com', '48988772211'),
+	('Janaina', '2000-01-04', 'janaina@gmail.com', '48988771100'),
+	('Antônio', '2000-01-05', 'antonio@gmail.com', '48988779988'),
+	('André', '2000-01-06', 'andre@gmail.com', '48988778877'),
+	('Anna', '2000-01-07', 'anna@gmail.com', '48988775566'),
+	('Anastacia', '2000-01-08', 'anastacia@gmail.com', '48988774433'),
+	('Bernardo', '2000-01-09', 'bernardo@gmail.com', '48988773322'),
+	('Bertoldo', '2000-01-10', 'bertoldo@gmail.com', '48988772211'),
+	('Bianca', '2000-01-11', 'bianca@gmail.com', '48988771100'),
+	('Beatriz', '2000-01-12', 'beatriz@gmail.com', '48988770000'),
+	('Carlos', '2000-01-13', 'carlos@gmail.com', '48988770099'),
+	('Cayo', '2000-01-14', 'cayo@gmail.com', '48988771199'),
+	('Carine', '2000-01-15', 'carine@gmail.com', '48988772288');
